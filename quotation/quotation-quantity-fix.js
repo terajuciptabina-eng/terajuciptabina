379c6ce3b897978d687456feab5733419d3304f0
Asyraf Reeza <terajuciptabina@gmail.com>
2026-09-07 12:34:13 +0800

    Route Build Contractor detailed quotation through frozen quotation output

--- quotation/quotation-quantity-fix.js
+++ quotation/quotation-quantity-fix.js
@@ -13,7 +13,11 @@
     load('tcBuildPlannerQuotationRenderer', 'buildplanner-alignment-fix.js?v=20260907-final', () => {
       load('tcBuildPlannerSimpleStructureFix', 'buildplanner-simple-structure-fix.js?v=20260907-final', () => {
         load('tcBuildPlannerDetailedFix', 'buildplanner-detailed-fix.js?v=20260907-final', () => {
-          if (contractor && !renovationContractor) load('tcContractorItemEditor', 'contractor-item-editor.js?v=20260907-1');
+          if (contractor && !renovationContractor) {
+            load('tcContractorItemEditor', 'contractor-item-editor.js?v=20260907-1', () => {
+              load('tcContractorDetailedQuotation', 'contractor-detailed-quotation.js?v=20260907-2');
+            });
+          }
           if (renovationContractor) load('tcRenovationContractorItemEditor', 'renovation-contractor-item-editor.js?v=20260907-1');
         });
       });
