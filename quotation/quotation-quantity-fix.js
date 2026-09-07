7e67d26cf231e5ea36c569c113b823f9302b523e
Asyraf Reeza <terajuciptabina@gmail.com>
2026-09-07 12:03:45 +0800

    Enable contractor item editor on renovation planner

--- quotation/quotation-quantity-fix.js
+++ quotation/quotation-quantity-fix.js
@@ -7,13 +7,14 @@
     s.onload = () => next?.();
     document.head.appendChild(s);
   }
+  const contractor = new URLSearchParams(location.search).get('audience') === 'contractor';
+  const renovationContractor = contractor && /renoplannercon\.html$/i.test(location.pathname);
   load('tcBuildPlannerStatePersistence', 'buildplanner-state-persistence.js?v=20260907-final', () => {
     load('tcBuildPlannerQuotationRenderer', 'buildplanner-alignment-fix.js?v=20260907-final', () => {
       load('tcBuildPlannerSimpleStructureFix', 'buildplanner-simple-structure-fix.js?v=20260907-final', () => {
         load('tcBuildPlannerDetailedFix', 'buildplanner-detailed-fix.js?v=20260907-final', () => {
-          if (new URLSearchParams(location.search).get('audience') === 'contractor') {
-            load('tcContractorItemEditor', 'contractor-item-editor.js?v=20260907-1');
-          }
+          if (contractor && !renovationContractor) load('tcContractorItemEditor', 'contractor-item-editor.js?v=20260907-1');
+          if (renovationContractor) load('tcRenovationContractorItemEditor', 'renovation-contractor-item-editor.js?v=20260907-1');
         });
       });
     });
