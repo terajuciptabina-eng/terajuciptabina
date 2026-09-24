06c405ea2e80e439bc5e47f582cbf8df7d3270fc
Asyraf Reeza <terajuciptabina@gmail.com>
2026-09-24 14:27:57 +0800

    Scope quotation API CORS to TERAJU Vercel deployments

--- api/quotations.js
+++ api/quotations.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const origin = String(req.headers.origin || '');
-  const allowedOrigin = origin === 'https://terajuciptabina-eng.github.io' || /^https:\/\/[^/]+\.vercel\.app$/.test(origin);
+  const allowedOrigin = origin === 'https://terajuciptabina-eng.github.io' || /^https:\/\/terajuciptabina-[a-z0-9]+-terajuciptabina-9880\.vercel\.app$/.test(origin);
   if (allowedOrigin) res.setHeader('Access-Control-Allow-Origin', origin);
   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
